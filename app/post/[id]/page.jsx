cf95c866ec714870f4ffea61f0a4e998c744d018
Mauri <mauri.monzon91@gmail.com>
2023-08-22 10:58:22 -0300

    Detail - componente por params

--- app/post/[id]/page.jsx
+++ app/post/[id]/page.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-
-function PostId() {
-  return <div>PostId</div>;
+async function loadPost(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await res.json();
+  return data;
+}
+async function PostId({ params }) {
+  const post = await loadPost(params.id);
+  return (
+    <div>
+      <h1>Post ID: {post.id}</h1>
+      <h3>Titulo: {post.title}</h3>
+      <p>Descripción: {post.body}</p>
+    </div>
+  );
 }
 
 export default PostId;
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,11 +1,15 @@
 "use client";
 
+import Link from "next/link";
+
 function PostCard({ post }) {
   return (
     <div>
-      <h3>
-        {post.id}. {post.title}
-      </h3>
+      <Link href={`/post/${post.id}`}>
+        <h3>
+          {post.id}. {post.title}
+        </h3>
+      </Link>
       <p>{post.body}</p>
       <button onClick={() => alert("Click funcionando")}>Click</button>
     </div>
