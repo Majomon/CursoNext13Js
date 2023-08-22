ac3134c201bc866c361793540953397bed659888
Mauri <mauri.monzon91@gmail.com>
2023-08-21 22:41:02 -0300

    Probando Layout

---
+++ app/shop/layout.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function ShopLayout({ children }) {
+  // Aca solamente retorn el children. No todo el HTML
+  return (
+    <>
+      <nav>
+        <h3>Sección tienda</h3>
+        <ul>
+          <li>
+            <Link href="/shop/category">Categorias</Link>
+          </li>
+          <li>
+            <Link href="/shop/category/computers">Computadoras</Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </>
+  );
+}
