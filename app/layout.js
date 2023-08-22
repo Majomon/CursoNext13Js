f9c7420085be8b36b1be240909151e461bbe9a30
Mauri <mauri.monzon91@gmail.com>
2023-08-21 22:31:12 -0300

    Movi carpeta components

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,10 @@
+import Navbar from "@/components/Navbar/Navbar";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <title>Curso de Next Js</title>
-      </head>
       <body>
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/shop">Shop</a>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
         {children}
       </body>
     </html>
