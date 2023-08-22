8e79f78ad0ff5cf859f6555a5df98c048127493c
Mauri <mauri.monzon91@gmail.com>
2023-08-22 16:02:35 -0300

    Creado api y algunas rutas

---
+++ src/app/api/users/route.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  // Puedo extraer parametros
+  // Puedo consultar una base de datos
+  // Puedo comunicarme con otros servidores
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return NextResponse.json({ data });
+}
+
+export function POST() {
+  return NextResponse.json({
+    message: "Creando datos",
+  });
+}
+
+export function PUT() {
+  return NextResponse.json({
+    message: "Modificando datos",
+  });
+}
+
+export function DELETE() {
+  return NextResponse.json({
+    message: "Eliminando datos",
+  });
+}
--- src/app/users/[usersId]/page.jsx
+++ src/app/users/[usersId]/page.jsx
@@ -3,12 +3,12 @@ import { useParams } from "next/navigation";
 
 function UsersPage({}) {
   const params = useParams();
-  console.log(params);
+
   return (
     <div>
       <button
         onClick={() => {
-          console.log("Works!!!");
+          console.log(`Usuario de ID: ${params.usersId}`);
         }}
       >
         Click
